ff1d6b35137c65357bdb0e4c3c54373a0b584427
PrabhavKondapally <kondapallyprabhav@gmail.com>
2026-06-11 18:56:41 +0000

    Sidebar fix

--- pages/Patient_Triage.py
+++ pages/Patient_Triage.py
@@ -1,5 +1,14 @@
 import streamlit as st
 
+col1, col2 = st.columns([1, 4])
+with col1:
+    st.image("cliniforge_logo.png", width=90)
+with col2:
+    st.markdown("<div style='padding-top: 15px;'></div>", unsafe_allow_html=True)
+    st.image("CliniForgeBanner.png", width=350)
+
+st.divider()
+
 st.title("📋 Clinical Triage System")
 st.header("📥 Patient Data Input")
 
